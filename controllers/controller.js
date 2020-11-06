a89699c5d5b4611fb62f0d9c4b922a011128ecae
IqbalSaimbhi <saimbhi.iqbal@gmail.com>
2020-11-06 09:08:31 -0500

    registerUser functionality

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,8 @@
 const sequelize = require("../config/sequelize").sequelize;
 const DataTypes = require("sequelize");
 const users = require("../models/users")(sequelize, DataTypes);
+const gcappemail = require("../models/GCAPPEMAIL")(sequelize, DataTypes);
+const validateRegisterInput = require("../validation/register");
 var md5 = require('md5');
 
 const getAllUsers = (req, res) => {
@@ -46,10 +48,53 @@ const userAuth = (req, res) => {
   
 }
 
+const register = function(req, res){
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  users.findOne({ where: {Login: req.body.Login }}).then(users => {
+    if (users) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+
+  userRegstr(req.body.Login, req.body.passwd, req.body.email);
+
+    }
+  });
+
+
+
+function userRegstr(Login, passwd, email){
+
+   users.sequelize.query("SET IDENTITY_INSERT dbo.users ON; INSERT INTO dbo.users (Login , passwd , sex, LoginUID, firstLogin, lastConnect, lastLogin, playTime, gamePoint, IPAddress, Connecting, ModeLevel, ChannelingID, Grade) VALUES('" + Login + "','" + passwd + "','0',(SELECT TOP 1 LoginUID FROM dbo.users ORDER BY LoginUID DESC) + 1, GETDATE(), GETDATE(),GETDATE(), 0, 30000, '0.0.0.0', 0, 0x000000, 0, 0)",
+   { type: sequelize.QueryTypes.INSERT }
+   ).then(function (result) {
+        if(result){
+        return res.json(200, {response: {code: 200, message:'User Registered Successfully'}});
+    } else{
+       res.json(400, {response: {code: 400, message: 'Error Occured while Registration'}});
+    } 
+})
+
+}
+
+
+
+}
+
+
+
+
 
 
 module.exports = {
   getAllUsers,
   getOneUser,
-  userAuth
+  userAuth,
+  register,
+  
+
 };
