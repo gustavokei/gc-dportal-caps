847a88104b17b6e24a3afcd86ba9374589d37b44
Gustavo Kei Amemiya <gustavokeia@gmail.com>
2020-11-16 02:30:19 -0300

    implemented upchar + fixed editchar

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,7 +2,7 @@ const sequelize = require("../config/sequelize").sequelize;
 const DataTypes = require("sequelize");
 const users = require("../models/users")(sequelize, DataTypes);
 const Characters = require("../models/Characters")(sequelize, DataTypes);
-var md5 = require('md5');
+var md5 = require("md5");
 
 const getAllUsers = (req, res) => {
   users.findAll().then((data) => {
@@ -24,50 +24,71 @@ const getOneUser = (req, res) => {
 };
 
 const userAuth = (req, res) => {
-     let { Login, passwd } = req.body;
-     users.findOne({
-      where: {
-       Login: Login
-      }
-     }).then(users => {
-       if (!users) {
-         return res.status(404).send("User not found");
-          }  else {
-          
-             if (md5(passwd)!==users.passwd) 
-              return res.status(404).send("Invalid password");
-            res
-            .status(200)
-            .send("Success"); 
-       }  
-   });
-  
-}
-
-const getCharacter = (req, res) => {
-  let {Login, CharType} = req.body;
-  Characters
-    .findAll({
+  let { Login, passwd } = req.body;
+  users
+    .findOne({
       where: {
         Login: Login,
-        CharType: CharType
-      }
-    }).then(Characters => {
-      if (!Characters){
-        return res.status(404).send("No Character found for this user");
+      },
+    })
+    .then((users) => {
+      if (!users) {
+        return res.status(404).send("User not found");
       } else {
-        if(CharType!==Characters.CharType) {
-         return res.status(404).send("Character do not exist");
-        } else {
-          res.status(200).json(data);
-        }
-      }  
+        if (md5(passwd) !== users.passwd)
+          return res.status(404).send("Invalid password");
+        res.status(200).send("Success");
+      }
     });
+};
+
+const getCharacter = (req, res) => {
+  let { Login, CharType } = req.body;
+  Characters.findAll({
+    where: {
+      Login: Login,
+      CharType: CharType,
+    },
+  }).then((data) => {
+    if (data == "") {
+      return res
+        .status(404)
+        .send(
+          "This user does not exist or CharType " + CharType + " is invalid"
+        );
+    } else {
+      res.status(200).json(data);
+    }
+  });
+};
+
+const updateCharacter = (req, res) => {
+  let { Login, CharType, Level, ExpS4, Promotion, Win, Lose } = req.body;
+  Characters.update(
+    { Level: Level, ExpS4: ExpS4, Promotion: Promotion, Win: Win, Lose: Lose },
+    {
+      where: {
+        Login: Login,
+        CharType: CharType,
+      },
+    }
+  ).then((data) => {
+    if (data == "") {
+      return res
+        .status(404)
+        .send(
+          "This user does not exist or CharType " + CharType + " is invalid"
+        );
+    } else {
+      res.status(200).json(data);
+    }
+  });
 };
 
 module.exports = {
   getAllUsers,
   getOneUser,
   userAuth,
-  getCharacter
+  getCharacter,
+  updateCharacter,
 };
--- routes/router.js
+++ routes/router.js
@@ -6,7 +6,8 @@ const ctrlMain = require("../controllers/controller");
 router.get("/all", ctrlMain.getAllUsers);
 router.get("/one/:login", ctrlMain.getOneUser);
 router.post("/auth", ctrlMain.userAuth);
-router.post("/editchar", ctrlMain.getCharacter);
+router.post("/getchar", ctrlMain.getCharacter);
+router.post("/upchar", ctrlMain.updateCharacter);
 
 //https://stackoverflow.com/questions/27465850/typeerror-router-use-requires-middleware-function-but-got-a-object
 module.exports = router;
