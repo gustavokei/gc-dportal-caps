5f719cadb4ce74491c100972780066830302f24b
Gustavo Kei Amemiya <gustavokeia@gmail.com>
2020-12-01 18:19:38 -0300

    additem - verify if itemid is valid

--- controllers/controller.js
+++ controllers/controller.js
@@ -118,45 +118,61 @@ const userAuth = (req, res) => {
     });
 };
 
+const { QueryTypes } = require("sequelize");
 const addItem = (req, res) => {
   sequelize
-    .query(
-      "WIGAWaitItem_insert_20130402 @01iLoginUID_input=:LoginUID, @02iCharType_input=:CharType, @03iItemID_input=:ItemID, @04iGradeID_input=:GradeID, @05iPeriod_input=:Period, @06iDuration_input=:Duration, @07iGetType_input=:GetType, @08iStatus_input=:Status, @09iItemLevel_input=:ItemLevel, @10iStrengthLevel_input=:StrengthLevel",
-      {
-        replacements: {
-          LoginUID: parseInt(req.params.loginuid),
-          CharType: -1,
-          ItemID: parseInt(req.params.itemid),
-          GradeID: 3,
-          Period: -1,
-          Duration: -1,
-          GetType: 0,
-          Status: 0,
-          ItemLevel: 0,
-          StrengthLevel: 0,
-        },
-      }
-    )
+    .query("SELECT GoodsName FROM GoodsInfoList WHERE GoodsID = :GoodsID", {
+      type: QueryTypes.SELECT,
+      replacements: {
+        GoodsID: parseInt(req.params.itemid),
+      },
+    })
     .then((result) => {
-      sequelize
-        .query(
-          "UIGAUserItem_merge_20130415 @01iLoginUID_input=:LoginUID, @02iItemID_input=:ItemID, @03iGradeID_input=:GradeID, @04iWIGAUID_input=:WIGAUID",
-          {
-            replacements: {
-              LoginUID: parseInt(req.params.loginuid),
-              ItemID: parseInt(req.params.itemid),
-              GradeID: 3,
-              WIGAUID: Object.values(
-                Object.values(Object.values(result)[0])[0]
-              )[0],
-            },
-          }
-        )
-        .then((result) => {
-          res
-            .status(200)
-            .send(Object.values(Object.values(Object.values(result)[0])[0])[0]);
-        });
+      console.log(result);
+      if (result == "") {
+        // invalid id
+        res.status(200).json("Error! Invalid ItemId");
+      } else {
+        const itemName = result;
+        sequelize
+          .query(
+            "WIGAWaitItem_insert_20130402 @01iLoginUID_input=:LoginUID, @02iCharType_input=:CharType, @03iItemID_input=:ItemID, @04iGradeID_input=:GradeID, @05iPeriod_input=:Period, @06iDuration_input=:Duration, @07iGetType_input=:GetType, @08iStatus_input=:Status, @09iItemLevel_input=:ItemLevel, @10iStrengthLevel_input=:StrengthLevel",
+            {
+              replacements: {
+                LoginUID: parseInt(req.params.loginuid),
+                CharType: -1,
+                ItemID: parseInt(req.params.itemid),
+                GradeID: 3,
+                Period: -1,
+                Duration: -1,
+                GetType: 0,
+                Status: 0,
+                ItemLevel: 0,
+                StrengthLevel: 0,
+              },
+            }
+          )
+          .then((result) => {
+            sequelize
+              .query(
+                "UIGAUserItem_merge_20130415 @01iLoginUID_input=:LoginUID, @02iItemID_input=:ItemID, @03iGradeID_input=:GradeID, @04iWIGAUID_input=:WIGAUID",
+                {
+                  replacements: {
+                    LoginUID: parseInt(req.params.loginuid),
+                    ItemID: parseInt(req.params.itemid),
+                    GradeID: 3,
+                    WIGAUID: Object.values(
+                      Object.values(Object.values(result)[0])[0]
+                    )[0],
+                  },
+                }
+              )
+              .then((result) => {
+                console.log(itemName);
+                res.status(200).json(itemName);
+              });
+          });
+      }
     });
 };
 
