5b06bfee79319ab7fd6f9cc15b185ef3c17ffd27
Gustavo Kei Amemiya <gustavokeia@gmail.com>
2022-10-10 21:33:51 -0300

    fix: trying to fix sequelize issue

--- controllers/shadow/register.js
+++ controllers/shadow/register.js
@@ -3,22 +3,22 @@ var md5 = require("md5");
 
 const register = (req, res) => {
   sequelize
-  .query(
-    "mup_create_user @strUserID_=:userId, @strPassword_=:pwd, @strUserName_=:userName, @strIDCardNum_=:idCardNum, @iOK=:iOk, @iUserUID=:iUserUid",
-    {
-      replacements: {
-        userId: req.body.userId,
-        pwd: md5(req.body.pwd),
-        userName: req.body.userName,
-        idCardNum: req.body.idCardNum,
-        iOK: req.body.iOk,
-        iUserUid: req.body.iUserUid,
-      },
-    }
-  )
-  .then((val) => {
-    res.status(200).json(val);
-  });
+    .query(
+      "mup_create_user @strUserID_=:userId, @strPassword_=:pwd, @strUserName_=:userName, @strIDCardNum_=:idCardNum, @iOK=:okCode, @iUserUID=:userUid",
+      {
+        replacements: {
+          userId: req.body.userId,
+          pwd: md5(req.body.pwd),
+          userName: req.body.userName,
+          idCardNum: req.body.idCardNum,
+          okCode: req.body.okCode,
+          userUid: req.body.userUid,
+        },
+      }
+    )
+    .then((val) => {
+      res.status(200).json(val);
+    });
 };
 
 module.exports = {
